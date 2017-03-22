8286cdaa0c14db147e1dd7479dc2be6eda7ff035
Elise St Hilaire and Sam Kirsch <elise+sam@>
2017-03-22 10:57:11 -0700

    stop nameshadowing content byt renaming it lines, finalize new post page

--- app/models/post.js
+++ app/models/post.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   title: DS.attr(),
-  content: DS.attr(),
+  lines: DS.attr(),
   date: DS.attr(),
   tags: DS.attr(),
   category: DS.attr()
