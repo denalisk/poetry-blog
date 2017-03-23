2ee06e8104c6ffdca49e14307ddb9523ab9fa72b
Sam Kirsch <samdkirsch2@gmail.com>
2017-03-23 13:10:00 -0700

    implement funcitonal checkForNewCategory logic within the new post form

--- app/routes/create-post.js
+++ app/routes/create-post.js
@@ -14,19 +14,34 @@ export default Ember.Route.extend({
 
   actions: {
     savePost() {
-      console.log("current title is " + this.controller.get('title'));
-      var params = {
-        author: this.controller.get('author'),
-        date: moment().format("MM/DD/YYYY"),
-        title: this.controller.get('title'),
-        lines: this.controller.get('lines').split("\n"),
-        tags: this.controller.get('tags').split(', '),
-        category: this.controller.get('category')
-      };
+      // var params = {
+      //   author: this.controller.get('author'),
+      //   date: moment().format("MM/DD/YYYY"),
+      //   title: this.controller.get('title'),
+      //   lines: this.controller.get('lines').split("\n"),
+      //   tags: this.controller.get('tags').split(', '),
+      //   category: this.controller.get('category')
+      // };
 
-      var newPost = this.store.createRecord('post', params);
-      newPost.save();
-      this.transitionTo('index');
+      var categoryParams = {
+        name: this.controller.get('category')
+      };
+      var store = this.store;
+      //
+      // var newPost = this.store.createRecord('post', params);
+      this.store.query('category', { orderBy: "name", equalTo: this.controller.get('category').toString(), limit: 1})
+      .then(function(result) {
+        if(!(result.get('length') > 0)) {
+          console.log("It's a new category, so new instance created");
+          var newCategory = store.createRecord('category', categoryParams);
+          newCategory.save();
+        } else {
+          console.log("That entry already exists");
+        }
+      });
+      // console.log(newCategory.get('id'));
+      // newPost.save();
+      // this.transitionTo('index');
     }
   }
 });
